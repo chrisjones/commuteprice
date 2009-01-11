537720458ead260c7945e5ba14e41031ce462eb5
Chris Jones <topher@chris-joness-imac.local>
2009-01-10 18:29:24 -0600

    formatting main container area

--- commuteprice.rb
+++ commuteprice.rb
@@ -5,3 +5,8 @@ require 'sinatra'
 get '/' do
   haml :index
 end
+
+post '/calculate' do
+  @origin, @dest, @mpg, @ppg = params[:origin], params[:dest], params[:mpg], params[:ppg]
+  haml :index
+end
