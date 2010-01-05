f279f36d2a967807124c73301cb95f6edc198768
unknown <chris@.harbert.net>
2010-01-05 17:19:39 -0600

    corrected google analytics api

--- public/javascript/google.js
+++ public/javascript/google.js
@@ -2,6 +2,6 @@ var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "htt
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 
 try{
-  var pageTracker = _gat._getTracker("UA-1911038-4");
+  var pageTracker = _gat._getTracker("UA-1911038-5");
   pageTracker._trackPageview();
 } catch(err) {}
